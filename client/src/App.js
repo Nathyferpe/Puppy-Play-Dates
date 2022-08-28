338d4044801fde9ef1ce7adbdf13e95072e7bf7f
“Nathalia <nathyferpe@gmail.com>
2022-08-28 18:11:43 -0400

    App.js - Routes added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Events from "./components/Events";
+import NavBar from "./components/NavBar";
+import Homepage from "./components/Homepage";
+import ProfilePageCreation from "./components/ProfilePageCreation";
+import GlobalStyles from "./components/GlobalStyles";
+import LoginPage from "./components/LoginPage";
+import ProfileFriend from "./components/ProfileFriend";
+import ProfilePage from "./components/ProfilePage"
+import Homegrid from "./components/Homegrid";
 
-function App() {
+const App = () => {
+
+  const [users, setUsers] = useState([])
+  const [user, setUser] = useState('')
+
+  // test //
+
+  localStorage.setItem("userId", "123");
+  const userId = localStorage.getItem("userId");
+  console.log("user id", userId);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <GlobalStyles />
+      <>
+        <NavBar 
+        />
+        <Switch>
+          <Route exact path="/">
+            <Homepage user={user} users={users} setUsers={setUsers}/>
+          </Route>
+          <Route exact path="/account/login">
+            <LoginPage user={user} users={users} setUsers={setUsers}/>
+          </Route>
+          <Route exact path="/account">
+            <ProfilePageCreation />
+          </Route>
+          <Route exact path="/homegrid">
+          <Homegrid/>
+          </Route>
+          <Route exact path="/profile/:email">
+            <ProfilePage />
+          </Route>
+          <Route exact path="/events">
+            <Events />
+          </Route>
+          <Route exact path="/users/:id">
+            <ProfileFriend />
+          </Route>
+        </Switch>
+      </>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
