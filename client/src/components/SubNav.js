13ff0a041cfd820a99d64e4b1d7933153330a143
“Nathalia <nathyferpe@gmail.com>
2022-08-28 18:21:37 -0400

    Sub-Navigation (In progress)

---
+++ client/src/components/SubNav.js
@@ -0,0 +1,23 @@
+import React from "react";
+import GlobalStyles from "./GlobalStyles";
+
+const SubNav = () => {
+
+    return (
+    <div style= {{display: "flex" }}>   
+            <div style={{display: "flex", justifyContent: "center"}}>
+                    <button style={{width: "200px"}}>My Profile</button>
+            </div>
+            <div style={{display: "flex", justifyContent: "center"}}>
+                <button style={{width: "200px"}}>Events</button>
+            </div>
+    </div>
+
+    );
+};
+
+export default SubNav;
+
+
+
+
