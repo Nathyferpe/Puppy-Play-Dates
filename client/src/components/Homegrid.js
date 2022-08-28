8e1b537fe224855145c8ab15a22099df77c39493
“Nathalia <nathyferpe@gmail.com>
2022-08-28 18:28:03 -0400

    HomeGrid collection in progress

--- client/src/components/Homegrid.js
+++ client/src/components/Homegrid.js
@@ -1,23 +1,76 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import styled from "styled-components";
-// import DogsGrid from "./GridHomePage/DogsGrid";
+import { HomeGridContext } from "./HomeGridContext";
+import { Link } from "react-router-dom";
+// import SubNav from "./SubNav";
+
 
 const Homegrid = () => {
-const [users, setUsers] = useState([])
+  const data = useContext(HomeGridContext);
+  const { users } = data;
+// const [users, setUsers] = useState([])
+
+//Friends 
+const [friendIds, setFriendIds] = useState([])
 
 
-  useEffect(() => {
-    fetch("/api/users/")
-      .then((res) => res.json())
-      .then((data) => {
-          setUsers(data.data)
-        console.log(data);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch(`/api/users/`)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //         setUsers(data.data)
+  //       console.log(data);
+  //     });
+  // }, []);
 
 
   console.log("users", users)
-  return <div>hola</div>;
+  return (
+  <Wrapper>
+      <h1>Add me to connect!</h1>
+    {/* <SubNav/> */}
+      <AllImages>
+        {users.map((listOfUsers, idx) => (
+          <li key={idx}>
+            <Link to={`/users/${listOfUsers.id}`}>
+              <img src={listOfUsers.avatarUrl} alt="usersProfileImages" />
+              <h2>{listOfUsers.name}</h2>
+              <h3>{listOfUsers.age}</h3>
+            </Link>
+            <div style={{display: "flex", justifyContent: "center"}}>
+                <button style={{width: "200px"}}>Friend me</button>
+            </div>
+          </li>
+        ))}
+      </AllImages>
+    </Wrapper>
+  )
 };
+
+const Wrapper = styled.div`
+width: var(--max-content-width);
+`;
+
+const AllImages = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  & li {
+    padding: 2px;
+  }
+  & img {
+    width:  300px;
+    height: 300px;
+    object-fit: cover;
+    /* width: 400px;
+    height: auto; */
+    border: 1px solid var(--primary-color);
+    &:hover {
+      border: 4px solid var(--primary-color);
+      transition: all 0.1s ease;
+    }
+  }
+  `;
+
 
 export default Homegrid;
