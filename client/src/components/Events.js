8b0d5edafdbd7455ee7c7aeae0ff3fbfdfdb18ee
“Nathalia <nathyferpe@gmail.com>
2022-08-28 18:02:46 -0400

    Events component created

---
+++ client/src/components/Events.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Events = () => {
+    return (
+        <div>
+            <h1 style={{textAlign: 'center'}}> Events</h1>
+            
+        </div>
+    )
+};
+
+export default Events;
