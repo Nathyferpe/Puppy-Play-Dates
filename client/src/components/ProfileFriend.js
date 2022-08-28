f0a21d36954f835bc03698d6c41b655dfed5ca3c
“Nathalia <nathyferpe@gmail.com>
2022-08-28 18:30:38 -0400

    FE - Profile Friend Page - created (raw)

---
+++ client/src/components/ProfileFriend.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProfileFriend = () => {
+  return  <h1 style={{textAlign: 'center'}}> Profile Friend Page - Puppy Play Dates</h1>
+};
+
+export default ProfileFriend;
