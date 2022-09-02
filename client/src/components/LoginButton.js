374ae624d200f74b22833b1e4d728a934869db31
“Nathalia <nathyferpe@gmail.com>
2022-09-01 23:04:40 -0400

    Changing style in Sing up Button

--- client/src/components/LoginButton.js
+++ client/src/components/LoginButton.js
@@ -1,11 +1,12 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import "./cssAuth0button.css";
 
 const LoginButton = () => {
     const {loginWithRedirect, isAuthenticated } = useAuth0()
 
         return (
             !isAuthenticated && (
-                <button onClick={() => loginWithRedirect()}>
+                <button className="btn" onClick={() => loginWithRedirect()}>
                     Sign In
                 </button>
             )
--- client/src/components/LogoutButton.js
+++ client/src/components/LogoutButton.js
@@ -5,7 +5,7 @@ const LogoutButton = () => {
 
         return (
             isAuthenticated && (
-                <button onClick={() => logout()}>
+                <button className="btn" onClick={() => logout()}>
                     Sign Out
                 </button>
             )
