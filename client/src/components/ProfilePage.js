487ca9ed4ea49cf89074860696c60cbe1ef7dea6
“Nathalia <nathyferpe@gmail.com>
2022-09-02 17:39:20 -0400

    FE - Styling a bit Profile

--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import SubNav from "./SubNav";
+import "./cssAuth0button.css";
+
 
 const ProfilePage = () => {
     const [currentUser, setCurrentUser] = useState([]);
@@ -17,14 +19,16 @@ const ProfilePage = () => {
 
                 console.log("data", info.data)
 
-                // info.data.friends.forEach((friend) => {
-                //     fetch(`/api/users/${friend}`)
-                //       .then((res) => res.json())
-                //       .then((info) => {
-                //         setFriends((prevArray) => [...prevArray, info.data]);
-                //       });
-                //   });
-        });
+                info.data.friends.forEach((friend) => {
+                    console.log("friends of the user" , info.data.friends);
+                    // console.log("friends", friends);
+                    fetch(`/api/users/${friends}`)
+                        .then((res) => res.json())
+                        .then((info) => {
+                            setFriends((prevArray) => [...prevArray, info.data]);
+                        });
+                    });
+            });
     }, [email]);
         
 console.log('email', email);
@@ -32,45 +36,61 @@ console.log('email', email);
 
 
     return (
+        <>
         <Container>
             <SubNav/>
             <Wrapper>
                 <img src={currentUser.avatarUrl} alt="user's profile" />
-                <p>{currentUser.name}</p>
-                <p>{currentUser.description}</p>
+                <p style={{fontSize: '50px' , margin: '20px 0 20px 0', fontsize: '5px', color: '#dd216b', fontWeight: '900'}}>{currentUser.name}</p>
+                <p className="description-user">{currentUser.description}</p>
             </Wrapper>
-            <FriendsList>
-            {currentUser?.friends?.length > 0 &&
-                friends.map((item) => {
-                    return <FriendsImgs src={item.avatarUrl} alt="Friends Profile" />;
-                })}
-            </FriendsList>  
         </Container>
+          <FriendsList>
+          {currentUser?.friends?.length > 0 &&
+              friends.map((item) => {
+                  return <FriendsImgs src={item.avatarUrl} alt="Friends Picture" />;
+              })}
+          </FriendsList> 
+          </> 
     );
 };
 
 const Container = styled.div`
-background: grey;
-width: 600px;
-height: 600px;
+background: #e5e1ed;
+display: flex;
+/* width: 600px; */
+/* height: 600px; */
 /* border-radius: 50%; */
 
 `;
 
 const Wrapper = styled.div`
-    top: 600px;
-    left: 600px;
+    /* top: 600px; */
+    /* margin-left: 40px; */
+    /* left: 600px; */
+    flex-direction: column;
     display: flex;
-    align-items: center
+    align-items: center;
+    /* margin-left: auto; */
+    margin-top: 50px;
+    margin-right: auto;
+
+        & img {
+        width:  500px;
+        height: 500px;
+        object-fit: cover;
+        margin: auto;
+        border-radius: 20px;
+        }
 `;
 
 const FriendsList = styled.div`
-position: absolute;
+/* position: absolute; */
   top: 700px;
   left: 320px;
   display: flex;
   align-items: center;
-  width: 60vw;
+  /* width: 60vw; */
   padding-bottom: 10px;
   border-bottom: 2px solid var(--primary-color);
   & p {
@@ -81,7 +101,9 @@ position: absolute;
 `;
 
 const FriendsImgs = styled.img`
-  width: var(--user-img-width);
+   width:  500px;
+        height: 500px;
+        object-fit: cover;
 `;
 
 export default ProfilePage;
