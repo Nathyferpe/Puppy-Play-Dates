1fc448a2411dbcff4c842d32e3de43c10e066e91
“Nathalia <nathyferpe@gmail.com>
2022-09-07 23:39:51 -0400

    FE - Styling in the HP

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,58 +1,73 @@
 import React from "react";
 import styled from "styled-components";
 // import DogsGrid from "./GridHomePage/DogsGrid";
-import doglineupbanner from "../assets/Global-images/doglineupbanner.jpeg"
+import doglineupbanner from "../assets/Global-images/doglineupbanner.jpeg";
 import { Link } from "react-router-dom";
 import HomePageEventSection from "./HomePageEventSection";
 // import { useAuth0 } from "@auth0/auth0-react";
 
 const Homepage = () => {
+  return (
+    <div>
+      <Banner>
+        <TextWrapper>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              flexDirection: "column",
+            }}
+          >
+            <h1
+              style={{
+                textAlign: "center",
+                color: "white",
+                padding: "0 0 40px 0",
+              }}
+            >
+              {" "}
+              Welcome to Puppy Play Connect{" "}
+            </h1>
+            <div style={{ display: "flex", width: "40vw" }}>
+              <h2 style={{ color: "#8c7ae6" }}>
+                We are a website were dogs looking to connect and have fun in
+                organized events.
+              </h2>
+            </div>
+            <div>
+              {/* <StyledNavLink to="/account"> Create a Profile </StyledNavLink> */}
+            </div>
+          </div>
+        </TextWrapper>
+        <HomePageEventSection />
+      </Banner>
+    </div>
 
-    return (
-
-        <div>
-            <Banner>
-                <TextWrapper>
-                <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column'}}>
-                <h1 style={{textAlign: 'center',  color: "white", padding: "0 0 40px 0"}}> Welcome to Puppy Play Connect </h1>
-                    <div style={{display: 'flex', width: "40vw"}}>
-                        <h2 style={{color: '#8c7ae6'}}>We are a website were dogs looking to connect and have fun in organized events.</h2>
-                    </div>
-                    <div>
-                    {/* <StyledNavLink to="/account"> Create a Profile </StyledNavLink> */}
-                    </div>
-                </div>
-                </TextWrapper>
-                <HomePageEventSection/>
-            
-            </Banner>
-        </div>
-
-        // )
-
-    )
+    // )
+  );
 };
 
 const Banner = styled.div`
-    background-image: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${doglineupbanner}); /* BANNER */
-    height: 100vh;
-    background-size: cover;
-    background-position: center;
-    background-attachment: fixed;
-    height: 100vh;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),
+    url(${doglineupbanner}); /* BANNER */
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+  height: 100vh;
 `;
 
 const TextWrapper = styled.div`
-/* border: solid #8c7ae6 5px; */
-padding-top: 250px;
-padding-bottom: 250px;
-padding-left: 100px;
-display: flex;
+  /* border: solid #8c7ae6 5px; */
+  padding-top: 250px;
+  padding-bottom: 250px;
+  padding-left: 100px;
+  display: flex;
   /* flex-wrap: wrap; */
-justify-content: left;
-width: 70vw;
-//border removed:
-/* border: white 2px solid; */
+  justify-content: left;
+  width: 70vw;
+  //border removed:
+  /* border: white 2px solid; */
 `;
 
 // const StyledNavLink = styled(Link)`
@@ -71,7 +86,6 @@ width: 70vw;
 //   width: 190px;
 //   text-decoration: none;
 //   transition: all ease 400ms;
-
 
 //   &:disabled {
 //     cursor: not-allowed;
