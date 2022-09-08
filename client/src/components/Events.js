fce952d4251373dbd10827e786af0b0aa91d0962
“Nathalia <nathyferpe@gmail.com>
2022-09-07 23:34:35 -0400

    FE- Events.js

--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -1,47 +1,67 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import styled from "styled-components";
 import doglineupbanner from "../assets/Global-images/doglineupbanner.jpeg";
-// import { data } from ''
-
+import { useParams } from "react-router-dom";
+// import { HomeGridContext } from "./HomeGridContext";
 
 const Events = () => {
-    // const { events, setEvents } = data;
+  //   const data = useContext(HomeGridContext);
+  const [events, setEvents] = useState([]);
+  const { id } = useParams();
 
+  useEffect(() => {
+    fetch(`/api/events/${id}`)
+      .then((res) => res.json())
+      .then((json) => {
+        setEvents(json.data);
 
-    // useEffect(() => {
-    //     fetch(`/api/events`)
-    //         .then((res) => res.json())
-    //         .then((json) => {
-    //             setEvents(json.data);
-    //         });
-    //     }, []);
+        console.log("data", json.data);
+      });
+  }, [id]);
 
-    return (
-        <div>
-            
-        <Banner>
-        <h1 style={{textAlign: 'center',  color: "white", padding: "80px 0 100px 0"}}> Events</h1>
-        <h2 style={{textAlign: 'left',  color: "white", padding: "40px 0 40px 40px"}}>Upcoming events...</h2>
-            <WrapperEvents>
-            <img></img>
-            </WrapperEvents>
-        </Banner>
-            
-        </div>
-    )
+  //   console.log("events", events);
+
+  return (
+    <div>
+      <Banner>
+        <h1
+          style={{
+            textAlign: "center",
+            color: "white",
+            padding: "80px 0 100px 0",
+          }}
+        >
+          Events
+        </h1>
+        <h2
+          style={{
+            textAlign: "left",
+            color: "white",
+            padding: "40px 0 40px 40px",
+          }}
+        >
+          Upcoming events...
+        </h2>
+        <WrapperEvents>
+          <img></img>
+        </WrapperEvents>
+      </Banner>
+    </div>
+  );
 };
 
 const Banner = styled.div`
-    background-image: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url(${doglineupbanner}); /* BANNER */
-    height: 100vh;
-    background-size: cover;
-    background-position: center;
-    background-attachment: fixed;
-    height: 100vh;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)),
+    url(${doglineupbanner}); /* BANNER */
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+  height: 100vh;
 `;
 
 const WrapperEvents = styled.div`
-display: flex;
+  display: flex;
 `;
 
 export default Events;
