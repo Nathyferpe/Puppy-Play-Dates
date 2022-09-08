5aefe8948a7f216ff3a6e537371e1032a72f9561
“Nathalia <nathyferpe@gmail.com>
2022-09-07 23:44:47 -0400

    FE - changing "" on DB1 & DB

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require("mongodb");
-require('dotenv').config()
+require("dotenv").config();
 // Connection URI
 require("dotenv").config();
 const { MONGO_URI } = process.env;
@@ -7,27 +7,26 @@ const users = require("./users.json");
 
 // const uri = process.env.MONGO_URL;
 const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 };
 
 const dbFunction = async (dbName) => {
-    // creates a new client
-    const client = new MongoClient(MONGO_URI, options);
+  // creates a new client
+  const client = new MongoClient(MONGO_URI, options);
 
-    // connect to the client
-    await client.connect();
+  // connect to the client
+  await client.connect();
 
-    // connect to the database (db name is provided as an argument to the function)
-    const db = client.db(dbName);
-    console.log("connected!");
+  // connect to the database (db name is provided as an argument to the function)
+  const db = client.db(dbName);
+  console.log("connected!");
 
-    await db.collection("users").insertMany(users);
+  await db.collection("users").insertMany(users);
 
-    // close the connection to the database server
-    client.close();
-    console.log("disconnected!");
+  // close the connection to the database server
+  client.close();
+  console.log("disconnected!");
 };
 
 // dbFunction("puppyplaydates");
-
--- server/db1.js
+++ server/db1.js
@@ -1,36 +1,32 @@
 const { MongoClient } = require("mongodb");
-require('dotenv').config()
+require("dotenv").config();
 // Connection URI
 require("dotenv").config();
 const { MONGO_URI } = process.env;
 const events = require("./events.json");
 
-
 // const uri = process.env.MONGO_URL;
 const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 };
 
 const dbFunctionEvents = async (puppyplaydates) => {
-    // creates a new client
-    const client = new MongoClient(MONGO_URI, options);
+  // creates a new client
+  const client = new MongoClient(MONGO_URI, options);
 
-    // connect to the client
-    await client.connect();
+  // connect to the client
+  await client.connect();
 
-    // connect to the database (db name is provided as an argument to the function)
-    const db = client.db(puppyplaydates);
-    console.log("connected!");
+  // connect to the database (db name is provided as an argument to the function)
+  const db = client.db(puppyplaydates);
+  console.log("connected!");
 
-    await db.collection("events").insertMany(events);
+  await db.collection("events").insertMany(events);
 
-    // close the connection to the database server
-    client.close();
-    console.log("disconnected!");
-    };
+  // close the connection to the database server
+  client.close();
+  console.log("disconnected!");
+};
 
 // dbFunctionEvents("puppyplaydates");
-
-
-
