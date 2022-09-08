24104adfcf93f4bd0553d8451d62f0da07816a5e
“Nathalia <nathyferpe@gmail.com>
2022-09-07 23:30:45 -0400

    FE - Auth0 truned ON

--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { HomeGridPageProvider } from "./components/HomeGridContext"
-import { Auth0Provider } from '@auth0/auth0-react';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { HomeGridPageProvider } from "./components/HomeGridContext";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 ReactDOM.render(
   <React.StrictMode>
-      {/* <Auth0Provider
+    <Auth0Provider
       domain={domain}
       clientId={clientId}
       redirectUri={window.location.origin}
-      > */}
-        <HomeGridPageProvider>
-          <App />
-        </HomeGridPageProvider>
-      {/* </Auth0Provider> */}
+    >
+      <HomeGridPageProvider>
+        <App />
+      </HomeGridPageProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
