b96e64da56de8bda0311ca0c2441a8ba2d3dcdc5
“Nathalia <nathyferpe@gmail.com>
2022-09-08 23:48:16 -0400

    FE - Button FriendMe - Fetch not working (in progress)

--- client/src/components/ButtonFriendMe.js
+++ client/src/components/ButtonFriendMe.js
@@ -3,34 +3,61 @@ import { useHistory } from "react-router-dom";
 import { useParams } from "react-router-dom";
 
 const ButtonFriendme = () => {
-  const [friendMeButton, setFriendmeButton] = useState(false);
+  const [friendMeButton, setFriendMeButton] = useState(false);
   const history = useHistory();
   const { friendId, userId } = useParams();
+  // const { userId } = req.params.id;
+  // const { friendId } = req.params.id;
+  const [friendRequest, setFriendRequest] = useState("");
+  const [pendingFriends, setPendingFriends] = useState("");
+  const [friends, setFriends] = useState("");
+  const [email, setEmail] = useState("");
+  const [id, setId] = useState("");
 
-  /// userId & FriendId????
+  const formData = {
+    friendRequest,
+    pendingFriends,
+    friends,
+    email,
+    id,
+  };
 
-  useEffect(() => {
-    fetch(`/api/friends/${userId}/${friendId}`)
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+
+    /// userId & FriendId????
+
+    fetch(`/api/friends/${userId}/${friendId}`, {
+      method: "PATCH",
+      body: JSON.stringify(formData),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
       .then((res) => res.json())
-      .then((json) => {
-        return json.data;
-      })
-      .then((data) => {
-        setFriendmeButton(data);
-      })
-      .then(() => {});
-  }, []);
+      .then((info) => {
+        setFriendMeButton(info.data);
+        history.push(`/profile/${email}`);
+
+        console.log("data", info.data);
+        console.log(formData);
+      });
+  };
+
+  // console.log(json.data);
 
   // const handleClick = () => {
   //   history.push(`/profile/${id}`);
   // };
 
-  console.log("friendId", friendId);
-
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
+    <form
+      onSubmit={handleSubmit}
+      style={{ display: "flex", justifyContent: "center" }}
+    >
       <button style={{ width: "200px" }}>Friend me</button>
-    </div>
+    </form>
   );
 };
 
