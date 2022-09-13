f46165f3e32d747f07b981108bde7306d234ee21
“Nathalia <nathyferpe@gmail.com>
2022-09-13 19:58:16 -0400

    FE - Clean up - Dog Grid

--- client/src/components/GridHomePage/DogPictures.js
+++ client/src/components/GridHomePage/DogPictures.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-// This renders as a div with an image TAG on it, with some style and the source being the props url coming from the props (from doglist component)
-
 const DogPictures = (props) => {
   return (
     <div class="child">
